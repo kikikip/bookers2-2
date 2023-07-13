d2f6f714b4f1bc66369c9d501c32f43715354405
kikikip <135789408+kikikip@users.noreply.github.com>
2023-07-13 22:10:19 +0000

    [in the middle]bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def new
     @book=Book.new
   end
@@ -53,5 +55,11 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body, :user_id)
+  end
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
 end
