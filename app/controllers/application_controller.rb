00ba17d9da15da4440b9068f069b7df883e473a5
kikikip <135789408+kikikip@users.noreply.github.com>
2023-07-15 10:36:28 +0000

    [in the middle]bookers2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    flash[:notice] = "Signed in successfully."
-    user_path(current_user)
+    if current_user
+      flash[:notice] = "Signed in successfully."
+      user_path(resource)
+    else
+      flash[:notice] = "Welcome! You have signed up successfully."
+      user_path(resource)
+    end
   end
   #新規登録用メッセージ Welcome! You have signed up successfully.
 
